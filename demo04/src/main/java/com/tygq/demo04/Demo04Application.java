39edb9f5f29a4ee4b40f46064ce338928073eb5d
sunweiguo <317758022@qq.com>
2019-08-04 14:34:13 +0800

    'demo04:完成springboot和kafka的整合'

---
+++ demo04/src/main/java/com/tygq/demo04/Demo04Application.java
@@ -0,0 +1,13 @@
+package com.tygq.demo04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo04Application.class, args);
+    }
+
+}
---
+++ demo04/src/test/java/com/tygq/demo04/Demo04ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tygq.demo04;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo04ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
